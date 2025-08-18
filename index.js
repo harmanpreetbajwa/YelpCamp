30ae845d3a2760b3b563d7f627258ceae44f32b8
harmanpreetbajwa <57508783+harmanpreetbajwa@users.noreply.github.com>
2025-08-18 17:34:40 -0400

    Added show and new routes

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const Campground = require('./models/campground.js');
 
 app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp').then( () => {
     console.log("Mongo listening at port 27017.");
 }).catch( () => {
     console.log('An issue encoutered while connecting to Mongo');
 });
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -22,6 +24,23 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async(req, res) => {
     const allCampgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds: allCampgrounds});
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.get('/campgrounds/:id', async(req, res) => {
+    const { id } = req.params; 
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', {campground});
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campData = req.body.campground;
+    const newcamp = new Campground(campData);
+    const { _id } = await newcamp.save();
+    res.redirect(`/campgrounds/${_id}`);
 });
 
 app.listen(8080, () => {
