82e7db6b39edb0f34db07c3d73069fd7ad63b51b
harmanpreetbajwa <57508783+harmanpreetbajwa@users.noreply.github.com>
2025-11-01 00:11:34 -0400

    full  maps implemenation using mapTiler apis

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Review = require('./reviews.js');
 const Schema = mongoose.Schema;
+const opts = { toJSON: { virtuals: true } };
 
 const ImageSchema = new Schema(
     {
@@ -48,8 +49,13 @@ const CampgroundSchema = new Schema(
                 type: Schema.Types.ObjectId,
                 ref: 'Review'
             }],
-    }
-);
+    },  opts );
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0, 20)}...</p>`
+});
 
 CampgroundSchema.post('findOneAndDelete', async function (doc){
     if (doc){
